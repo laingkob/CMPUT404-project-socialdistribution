1ec5fc9d52323a3b5cce22e7c4a6f0c3e81fac3c
Hua Wang <hua6@ualberta.ca>
2023-02-27 19:52:25 -0700

    sepreate follow files and add tests

--- socialdistribution/service/admin.py
+++ socialdistribution/service/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from service.models import author, post
+from service.models import author, post, follow
 
 # Register your models here.
 
 admin.site.register(author.Author)
-admin.site.register(author.Followers)
+admin.site.register(follow.Followers)
 #admin.site.register(post.Post) #we don't need this here, but leaving as comment just in case
 #admin.site.register(post.Category)
 
--- socialdistribution/service/models/author.py
+++ socialdistribution/service/models/author.py
@@ -24,7 +24,4 @@ class Author(models.Model):
             "profileImage": self.profileImage,
     }
     
-class Followers(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="all_authors")
-    follower = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="all_followers")
 
