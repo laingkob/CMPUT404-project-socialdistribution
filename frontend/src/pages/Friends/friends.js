c7d362298736c1ec990b16b364e6dfa40db85c07
Hua Wang <hua6@ualberta.ca>
2023-03-03 03:47:15 -0700

    add friend page fix

--- frontend/src/pages/Friends/friends.js
+++ frontend/src/pages/Friends/friends.js
@@ -2,9 +2,9 @@
 import { get_authors }from '../../api/author_api'
 import { get_author }from '../../api/author_api'
 import { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { useSelector } from "react-redux"
 import { add_followers_for_author } from '../../api/follower_api';
-import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import * as React from "react";
 import Table from "@mui/material/Table";
@@ -16,37 +16,54 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+
 function Friends() {
 
-    var { author_id } = useParams();
-    author_id = useSelector((state) => state.user).id;
-    console.log("Starting page for :", author_id);
+    const user = useSelector((state) => state.user);
+    const author_id = `http://localhost/authors/${user.id}/`
     const [author, setAuthor] = useState({});
     const [follow_list, setList] = useState({"items": []}); 
     const [success, setSucess] = useState(null); 
+    const navigate = useNavigate();
+    
 
-    let page = 1;
-  
+    
+    var page = 1;
+
     useEffect(() => { 
-      console.log("Using effect");
+      get_author(`http://localhost/authors/${user.id}/`, setAuthor);
+  
+      get_authors(page,setList)
       
-      get_author(author_id, setAuthor);
-      get_authors(page, setList)
-      
-    }, [author_id, page]);
+  
+    }, []);
 
   
-    /*const followAuthor = async (follow_id) => {
-      console.log("Inside followAuthor");
-        await add_followers_for_author(`${author_id}`,follow_id, setSucess(true))
-    }*/
+    const followAuthor= (follow_id) => {
+
+        add_followers_for_author(user.id, follow_id, onSuccess)
+    }
+
+    const onSuccess = () => {
+        setSucess(true);
+    }
     
     const page_buttons = () => {
-        if (follow_list.items.length < 5 && page === 1)
+
+    /*  need fix here!
+ 
+        if (follow_list.items.length < 5 && page == 1)
         {
           return;
-        } 
-        else if (page === 1)
+        }
+         
+        if (page == 1)
         {
             return (<button onClick={forward_page}>Next Page</button>);
         } 
@@ -62,22 +79,59 @@ function Friends() {
             <button onClick={forward_page}>Next Page</button>
           </div>);
         }
+        */
+         
+        return (
+            <div>
+              <button onClick={back_page}>Prev Page</button>
+              <button onClick={forward_page}>Next Page</button>
+            </div>);
+       
       };
 
 
     const forward_page = () => {
         page = page + 1;
         get_authors(page,setList)
+        
       };
     
     const back_page = () => {
+        if (page == 1)
+        {
+            page=page + 1;
+        } 
         page = page - 1;
         get_authors(page,setList)
+        
+      };
+    
+    const goBack = () => {
+        navigate("/");
+        
       };
   
   
     return (
+        
         <>
+        <div>
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Button
+                variant="contained"
+                onClick={goBack}
+                >
+                back
+            </Button>
+          <Typography variant="h6" align="left" color="inherit" component="div">
+            Add friends
+          </Typography>
+          </Toolbar>
+        </AppBar>
+        </Box>
+        </div>
         <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -100,7 +154,7 @@ function Friends() {
                 <TableCell align="right">
                   <Button
                     variant="contained"
-                    /*onClick={followAuthor(row.id)}*/
+                    onClick={(e) => followAuthor(row.id)}
                   >
                     follow
                   </Button>
@@ -113,6 +167,8 @@ function Friends() {
       <div style={{ width: "100%", textAlign: "center", paddingTop: 16 }}>
           {page_buttons()}
       </div>
+      
+
       </>
     );
   }
