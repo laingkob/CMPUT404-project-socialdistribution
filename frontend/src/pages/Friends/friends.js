5e38e2cb91861f343ac17873e5990e1330b9b5dd
Aidan Horemans <aidanjhoremans@gmail.com>
2023-03-03 12:17:27 -0700

    fix page buttons

--- frontend/src/pages/Friends/friends.js
+++ frontend/src/pages/Friends/friends.js
@@ -4,7 +4,7 @@ import { get_author }from '../../api/author_api'
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux"
 import { add_followers_for_author } from '../../api/follower_api';
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import * as React from "react";
 import Table from "@mui/material/Table";
@@ -27,42 +27,44 @@ function Friends() {
 
     const user = useSelector((state) => state.user);
     const author_id = `http://localhost/authors/${user.id}/`
-    const [author, setAuthor] = useState({});
     const [follow_list, setList] = useState({"items": []}); 
-    const [success, setSucess] = useState(null); 
+    const [success, setSuccess] = useState(null); 
     const navigate = useNavigate();
     
+    let page = 1;
 
-    
-    var page = 1;
+    const location = useLocation();
 
     useEffect(() => { 
-      get_author(`http://localhost/authors/${user.id}/`, setAuthor);
-  
-      get_authors(page,setList)
+      get_authors(page, setList)
       
-  
     }, []);
 
+    const get_search_params = () => {
+      const queryParams = new URLSearchParams(location.search);
+      const query_page = queryParams.get("page");
   
-    const followAuthor= (follow_id) => {
+      if (query_page) page = parseInt(query_page);
+    };
+    
+    get_search_params();
 
+    const followAuthor= (follow_id) => {
         add_followers_for_author(user.id, follow_id, onSuccess)
     }
 
     const onSuccess = () => {
-        setSucess(true);
+        setSuccess(true);
     }
     
     const page_buttons = () => {
 
-    /*  need fix here!
+    /*  need fix here! */
  
         if (follow_list.items.length < 5 && page == 1)
         {
           return;
         }
-         
         if (page == 1)
         {
             return (<button onClick={forward_page}>Next Page</button>);
@@ -79,36 +81,23 @@ function Friends() {
             <button onClick={forward_page}>Next Page</button>
           </div>);
         }
-        */
-         
-        return (
-            <div>
-              <button onClick={back_page}>Prev Page</button>
-              <button onClick={forward_page}>Next Page</button>
-            </div>);
-       
       };
 
 
     const forward_page = () => {
         page = page + 1;
-        get_authors(page,setList)
-        
+        navigate(`/friends/?page=${page}`);
+        navigate(0)
       };
     
     const back_page = () => {
-        if (page == 1)
-        {
-            page=page + 1;
-        } 
         page = page - 1;
-        get_authors(page,setList)
-        
+        navigate(`/friends/?page=${page}`);
+        navigate(0)
       };
     
     const goBack = () => {
         navigate("/");
-        
       };
   
   
