dcddbcaa4efd0c84b6176f5a22a37a51b1903a10
Aidan Horemans <aidanjhoremans@gmail.com>
2023-03-22 21:10:22 -0600

    fix profile fetching and some other null checks

--- service/urls.py
+++ service/urls.py
@@ -10,8 +10,12 @@ from service.views.comment import CommentView
 from service.views.liked import LikedView, LikesView
 from django.views.generic import TemplateView
 
-#this shit is hell, but django freaks out trying to parse nested urls otherwise
-AUTHOR_ID_REGEX = r"https://[A-Za-z0-9\w.-]+/authors/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}"
+# this shit is hell, but django freaks out trying to parse nested urls otherwise
+
+HOST_REGEX = r"[A-Za-z0-9\w.-]+"
+GUID_REGEX = r"[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}"
+
+AUTHOR_ID_REGEX = rf"https://{HOST_REGEX}/authors/{GUID_REGEX}"
 POST_ID = r"https://[A-Za-z0-9\w.-]+/authors/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/posts/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}"
 COMMENT_ID_REGEX = r"https://[A-Za-z0-9\w.-]+/authors/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/posts/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/comments/[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}"
 
