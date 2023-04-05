2d9839efcb39389055bf6a88b699c6dbbbd2b930
Junesung Lee <junesoung412@gmail.com>
2023-04-04 20:43:06 -0600

    redirect user to originally reuqested url after sign in

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -17,29 +17,30 @@ function Sidebar() {
   //Handle Navigations
   const navigate = useNavigate();
   //Navigate to Stream
-  const goToStream = () =>{
-      navigate("/");
+  const goToStream = () => {
+    navigate("/");
   }; //Navigate to Main Inbox Feed
-  const goToInbox = () =>{
-      navigate("/inbox");
-  };//Navigate to Add Friends page
+  const goToInbox = () => {
+    navigate("/inbox");
+  }; //Navigate to Add Friends page
   const goToAdd = () => {
-      navigate("/friends/");
-  };//Navigate to Requests page
+    navigate("/friends/");
+  }; //Navigate to Requests page
   const goToRequests = () => {
-      navigate("/friends/requests");
+    navigate("/friends/requests");
   }; //Navigate to Post creation page
   const goToNewPost = () => {
-      navigate("/posts/new");
+    navigate("/posts/new");
   }; //Navigate to Upload Image page
   const goToImageUpload = () => {
-      navigate("/posts/image");
-  }
-  
+    navigate("/posts/image");
+  };
+
   //Signout functionality
   const dispatch = useDispatch();
   const signOut = () => {
     dispatch(clearUser(user));
+    goToStream();
   };
 
   //TODO: Attach to backend to pull actual numbers
@@ -49,7 +50,11 @@ function Sidebar() {
       <div className="Sidebar">
         {/* Profile Preview */}
         <div className="Profile" id="profile-tab">
-          <img className="profile-pic" src={user.profileImage === "" ? profile : user.profileImage} alt="profile" />
+          <img
+            className="profile-pic"
+            src={user.profileImage === "" ? profile : user.profileImage}
+            alt="profile"
+          />
           <Link to={authorUrl + "/edit"}>
             <EditButton />
           </Link>
@@ -71,36 +76,50 @@ function Sidebar() {
         </div>
         {/* Navigation Menu */}
         <menu>
-            <li>
-                <button className='Page' id="stream" onClick={goToStream}>Stream</button>
-                {/* Inbox Options Submenu, only show if Inbox selected */}
-                <ul className="Options-bar">
-                    <li>
-                        <button className='Options' id="inbox" onClick={goToInbox}>Inbox</button>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <button className='Page' onClick={goToAdd}>Add Friends</button>
-                {/* Requests Options Submenu, only show if Friends selected */}
-                <ul className="Options-bar">
-                    <li>
-                        <button className='Options' onClick={goToRequests}>Pending</button>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <button className='Page' onClick={goToNewPost} id="new">New Post</button>
-                {/* Post Options Submenu, only show if Post selected */}
-                <ul className="Options-bar">
-                    <li>
-                        <button className='Options' onClick={goToImageUpload}>Upload Image</button>
-                    </li>
-                </ul>
-            </li>
-            <li>
-            <button className='Page' onClick={signOut}>Sign Out</button>
-            </li>
+          <li>
+            <button className="Page" id="stream" onClick={goToStream}>
+              Stream
+            </button>
+            {/* Inbox Options Submenu, only show if Inbox selected */}
+            <ul className="Options-bar">
+              <li>
+                <button className="Options" id="inbox" onClick={goToInbox}>
+                  Inbox
+                </button>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <button className="Page" onClick={goToAdd}>
+              Add Friends
+            </button>
+            {/* Requests Options Submenu, only show if Friends selected */}
+            <ul className="Options-bar">
+              <li>
+                <button className="Options" onClick={goToRequests}>
+                  Pending
+                </button>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <button className="Page" onClick={goToNewPost} id="new">
+              New Post
+            </button>
+            {/* Post Options Submenu, only show if Post selected */}
+            <ul className="Options-bar">
+              <li>
+                <button className="Options" onClick={goToImageUpload}>
+                  Upload Image
+                </button>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <button className="Page" onClick={signOut}>
+              Sign Out
+            </button>
+          </li>
         </menu>
       </div>
       {/*<div className='Right Sidebar'>
